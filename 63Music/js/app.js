d3c4edebdf388d682a020843578c1d81ae64a02e
uglysixthree <163ogfame@gmail.com>
2024-12-23 22:00:38 +0300

    second commit change

---
+++ 63Music/js/app.js
@@ -0,0 +1,76 @@
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+
+if (ScrollTrigger.isTouch !==1) {
+
+    ScrollSmoother.create({
+        wrapper: '.wrapper',
+        content: '.content',
+        smooth: 1.5,
+        effect: true
+    })
+
+    gsap.fromTo('.main_content', { opacity: 1 }, {
+        opacity: 0, 
+        scrollTrigger: {
+            trigger: '.main_content',
+            start: '100',
+            end: '500',
+            scrub: true
+        }
+    })
+
+    gsap.fromTo('.services_h1', { y: 100, opacity: 0.5 }, {
+        opacity: 1,
+        y: 0,
+        scrollTrigger: {
+            trigger: '.services_h1',
+            scrub: true
+        }
+    })
+
+    gsap.fromTo('.card-1', { x: -100, opacity: 0.75 }, {
+        opacity: 1,
+        x: 0,
+        scrollTrigger: {
+            trigger: '.card-1',
+            scrub: true
+        }
+    })
+
+    gsap.fromTo('.card-2', { x: -50, opacity: 0.75 }, {
+        opacity: 1,
+        x: 0,
+        scrollTrigger: {
+            trigger: '.card-2',
+            scrub: true
+        }
+    })
+
+    gsap.fromTo('.card-3', { x: 50, opacity: 0.75 }, {
+        opacity: 1,
+        x: 0,
+        scrollTrigger: {
+            trigger: '.card-3',
+            scrub: true
+        }
+    })
+
+    gsap.fromTo('.card-4', { x: 100, opacity: 0.75 }, {
+        opacity: 1,
+        x: 0,
+        scrollTrigger: {
+            trigger: '.card-4',
+            scrub: true
+        }
+    })
+
+    gsap.fromTo('.contact', { x: -100, y: 100, opacity: 0.5 }, {
+        opacity: 1,
+        y: 0,
+        x: 0,
+        scrollTrigger: {
+            trigger: '.contact',
+            scrub: true
+        }
+    })
+}
---
+++ 63Music/js/script.js
@@ -0,0 +1,99 @@
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        let href = this.getAttribute('href').substring(1);
+        const scrollTarget = document.getElementById(href);
+
+        if (!scrollTarget) return; // Прекратить, если цель отсутствует
+
+        const topOffset = document.querySelector('header').offsetHeight || 0;
+        const elementPosition = scrollTarget.getBoundingClientRect().top;
+        const offsetPosition = elementPosition - topOffset;
+
+        window.scrollBy({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    });
+});
+
+const card1 = document.getElementById('card-1'); // Исправлено
+const card1h2 = document.getElementById('card-1_h2'); // Исправлено
+const card1img = document.getElementById('card1img');
+
+card1.addEventListener('mouseover', () => {
+    card1h2.style.opacity = '1'; // Сделать видимым
+    card1.style.transform = 'rotate(4deg)';
+    card1img.style.width = '380px';
+    card1img.style.height = '220px';
+    card1img.style.filter = 'blur(2px)';
+});
+
+card1.addEventListener('mouseout', () => {
+    card1h2.style.opacity = '0'; // Скрыть
+    card1.style.transform = 'rotate(0deg)';
+    card1img.style.width = '360px';
+    card1img.style.height = '200px';
+    card1img.style.filter = 'blur(0px)';
+});
+
+const card2 = document.getElementById('card-2'); // Исправлено
+const card2h2 = document.getElementById('card-2_h2'); // Исправлено
+const card2img = document.getElementById('card2img');
+
+card2.addEventListener('mouseover', () => {
+    card2h2.style.opacity = '1'; // Сделать видимым
+    card2.style.transform = 'rotate(-4deg)';
+    card2img.style.width = '380px';
+    card2img.style.height = '220px';
+    card2img.style.filter = 'blur(2px)';
+});
+
+card2.addEventListener('mouseout', () => {
+    card2h2.style.opacity = '0'; // Скрыть
+    card2.style.transform = 'rotate(0deg)';
+    card2img.style.width = '360px';
+    card2img.style.height = '200px';
+    card2img.style.filter = 'blur(0px)';
+});
+
+const card3 = document.getElementById('card-3'); // Исправлено
+const card3h2 = document.getElementById('card-3_h2'); // Исправлено
+const card3img = document.getElementById('card3img');
+
+card3.addEventListener('mouseover', () => {
+    card3h2.style.opacity = '1'; // Сделать видимым
+    card3.style.transform = 'rotate(4deg)';
+    card3img.style.width = '380px';
+    card3img.style.height = '220px';
+    card3img.style.filter = 'blur(2px)';
+});
+
+card3.addEventListener('mouseout', () => {
+    card3h2.style.opacity = '0'; // Скрыть
+    card3.style.transform = 'rotate(0deg)';
+    card3img.style.width = '360px';
+    card3img.style.height = '200px';
+    card3img.style.filter = 'blur(0px)';
+});
+
+const card4 = document.getElementById('card-4'); // Исправлено
+const card4h2 = document.getElementById('card-4_h2'); // Исправлено
+const card4img = document.getElementById('card4img');
+
+card4.addEventListener('mouseover', () => {
+    card4h2.style.opacity = '1'; // Сделать видимым
+    card4.style.transform = 'rotate(-4deg)';
+    card4img.style.width = '380px';
+    card4img.style.height = '220px';
+    card4img.style.filter = 'blur(2px)';
+});
+
+card4.addEventListener('mouseout', () => {
+    card4h2.style.opacity = '0'; // Скрыть
+    card4.style.transform = 'rotate(0deg)';
+    card4img.style.width = '360px';
+    card4img.style.height = '200px';
+    card4img.style.filter = 'blur(0px)';
+});
