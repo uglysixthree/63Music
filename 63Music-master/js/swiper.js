dc75f5b661391b65f00aea0572bddfe083252d04
uglysixthree <163ogfame@gmail.com>
2025-01-21 21:21:14 +0300

    asddwehiodnfnfgsdk

--- 63Music-master/js/swiper.js
+++ 63Music-master/js/swiper.js
@@ -1,6 +1,6 @@
 const swiper = new Swiper('.swiper-container', {
   slidesPerView: 3, // Показывать 3 слайда одновременно
-  spaceBetween: 10,  // Отступы между слайдами
+  spaceBetween: 30,  // Отступы между слайдами
   loop: true,        // Бесконечная прокрутка
   autoplay: {
     delay: 3000,     // Автопрокрутка каждые 3 секунды
